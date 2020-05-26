a7907e65ba2e7573a836567dd5ad80acdb1134b8
1cecreamm <teyengkuroba@gmail.com>
2020-05-26 23:35:49 +0700

    Syberia-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -22,9 +22,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_platina.mk
+    $(LOCAL_DIR)/syberia_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_platina-eng \
-    xtended_platina-userdebug \
-    xtended_platina-user
+    syberia_platina-eng \
+    syberia_platina-userdebug \
+    syberia_platina-user
